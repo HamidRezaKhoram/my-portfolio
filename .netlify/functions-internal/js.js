8f58c20df27a43a9ff2b0bf1c40da34828460b07
HamidReza <hr.kh1380@gmail.com>
2022-07-06 19:27:21 +0430

    modified:   .netlify/functions-internal/index.js 	modified:   .netlify/functions-internal/js.js 	new file:   .netlify/multipart-parser-d6fbdf05.js 	modified:   .netlify/server/manifest.js 	modified:   .netlify/serverless.js 	modified:   .netlify/shims.js 	modified:   package-lock.json 	modified:   package.json

--- .netlify/functions-internal/js.js
+++ .netlify/functions-internal/js.js
@@ -5,7 +5,7 @@ exports.handler = init({
 	assets: new Set(["favicon.png"]),
 	mimeTypes: {".png":"image/png"},
 	_: {
-		entry: {"file":"start-e9998e2b.js","js":["start-e9998e2b.js","chunks/index-0b8a7a3e.js"],"css":[]},
+		entry: {"file":"start-6de1f6a2.js","js":["start-6de1f6a2.js","chunks/index-0b8a7a3e.js"],"css":[]},
 		nodes: [
 			() => Promise.resolve().then(() => require('../server/nodes/0.js')),
 			() => Promise.resolve().then(() => require('../server/nodes/1.js'))
