d0df3114fd0b6dd93f6051c5ac61df03ae206498
HamidReza <hr.kh1380@gmail.com>
2022-07-23 18:31:46 +0430

    modified:   src/routes/SecondSectionOfText.svelte

--- src/routes/SecondSectionOfText.svelte
+++ src/routes/SecondSectionOfText.svelte
@@ -1,50 +1,44 @@
-
-
-    <h3 class="c-article__heading">
-
-      <!-- svelte-ignore a11y-invalid-attribute -->
-      <a
-        >Experienced working with <a class="swift">Swift</a>,
-        <a class="js">Js</a>,
-        , <a class="linux">Linux🐧</a> Frontend & Backend.</a
-      >
-    </h3>
-
-  
+<h3 class="c-article__heading">
+  <!-- svelte-ignore a11y-invalid-attribute -->
+  <a
+    >Experienced working with <a class="swift">Swift</a>,
+    <a class="js">Js</a>, <a class="linux">Linux🐧</a> Frontend & Backend.</a
+  >
+</h3>
 
 <style>
-    @import url("https://fonts.googleapis.com/css2?family=Roboto:wght@900&display=swap");
-    .swift {
-      background: #9a9a9a;
+  @import url("https://fonts.googleapis.com/css2?family=Roboto:wght@900&display=swap");
+  .swift {
+    background: #9a9a9a;
 
-      border: 2px solid #9a9a9a;
-      border-radius: 5px;
-      font-family: "Roboto", sans-serif;
-      color: -webkit-linear-gradient(#eee, #ff6a32);
-      /* -webkit-background-clip: text; */
-      /* -webkit-text-fill-color: transparent; */
-    }
-    .js {
-      background: #f7e018;
-      color: #2d2f27;
-      border: 1px solid #f7e018;
-      border-radius: 5px;
-    }
-    .css {
-      background: -webkit-linear-gradient(#173459, #27a8e1);
-      -webkit-background-clip: text;
-      -webkit-text-fill-color: transparent;
-    }
-    .html {
-      background: -webkit-linear-gradient(#fc490b, black);
-      -webkit-background-clip: text;
-      -webkit-text-fill-color: transparent;
-    }
-    
-    .linux {
-      background: black;
-      color: white;
-      border: 2px solid black;
-      border-radius: 5px;
-    }
-  </style>
+    border: 2px solid #9a9a9a;
+    border-radius: 5px;
+    font-family: "Roboto", sans-serif;
+    color: -webkit-linear-gradient(#eee, #ff6a32);
+    /* -webkit-background-clip: text; */
+    /* -webkit-text-fill-color: transparent; */
+  }
+  .js {
+    background: #f7e018;
+    color: #2d2f27;
+    border: 1px solid #f7e018;
+    border-radius: 5px;
+  }
+  .css {
+    background: -webkit-linear-gradient(#173459, #27a8e1);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+  .html {
+    background: -webkit-linear-gradient(#fc490b, black);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+
+  .linux {
+    background: black;
+    color: white;
+    border: 2px solid black;
+    border-radius: 5px;
+  }
+</style>
