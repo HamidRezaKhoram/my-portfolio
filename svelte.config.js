b52a8d4413255837ff0a1565b6c51d69c8c276f2
HamidReza <hr.kh1380@gmail.com>
2022-07-23 14:13:58 +0430

    modified:   src/app.html 	renamed:    src/lib/fav3.png -> static/fav3.png 	modified:   svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,9 @@ import preprocess from 'svelte-preprocess';
 import { less } from 'svelte-preprocess-less';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	files: {
+		assets: 'static'
+	},
 	
 	preprocess: preprocess({
 		style: less(),
